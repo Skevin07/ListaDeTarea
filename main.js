94471c1070d445054a8f774769d194c0146da2ac
Kevin Rodriguez <kevinportillo808@gmail.com>
2024-07-23 23:41:07 -0600

    Final 1.0

--- main.js
+++ main.js
@@ -3,19 +3,47 @@ const addBtn = document.querySelector('.btn-add');
 const ul = document.querySelector('ul');
 const empty = document.querySelector('.empty');
 
-addBtn.addEventListener("click",(e)=>{
-e.preventDefault();
-const text = input.value;
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const text = input.value;
 
-const li = document.createElement('li');
-const p = document.createElement('p');
-p.textContent=text;
-li.appendChild(p);
-ul.appendChild(li);
+    if (text !== '') 
+    {
+        const li = document.createElement('li');
+        const p = document.createElement('p');
 
-})
 
-function addDeleteBtn (){
+        p.textContent = text;
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
+
+        input.value = "";
+        empty.style.display = "none";
+    }
+
+
+});
+
+function addDeleteBtn() {
     const DeleteBnt = document.createElement('button');
-    
+
+    DeleteBnt.textContent = "X";
+    DeleteBnt.className = "btn-delete";
+
+    DeleteBnt.addEventListener('click', (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+
+
+        const items = document.querySelectorAll('li');
+
+        if(items.length === 0){
+            empty.style.display = "block"
+        }
+
+
+
+    });
+    return DeleteBnt
 }
